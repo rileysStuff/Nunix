25d5a40678ac840e26f9f243eba68a5528f9401f
0x16000 <openbsd.gnu@gmail.com>
2025-04-15 22:26:29 +0200

    Current kernel version

--- include/version/version.h
+++ include/version/version.h
@@ -5,7 +5,7 @@
 #define BUNIX_NAME "Bunix"
 #define BUNIX_HOSTNAME "root"
 #define BUNIX_ARCH "i386"
-#define BUNIX_BUILD_VERSION "0.0.5-20250415"
+#define BUNIX_BUILD_VERSION "0.0.6-20250415"
 #define BUNIX_OS "Bunix"
 
 #endif /* _BUNIX_VERSION_H */
